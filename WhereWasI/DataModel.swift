9673e9818cea69d4cfb4a918d1db4a8e685ee3a8
Alex Slatinin <sandroisu1411@gmail.com>
2025-04-13 08:24:16 +0300

    added location data model

---
+++ WhereWasI/DataModel.swift
@@ -0,0 +1,17 @@
+//
+//  DataModel.swift
+//  WhereWasI
+//
+//  Created by Александр Слатинин on 13/04/2025.
+//
+
+class VisitedPoint{
+    
+    var latitude: String
+    var longitude: String
+    
+    init(latitude: String, longitude: String) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
